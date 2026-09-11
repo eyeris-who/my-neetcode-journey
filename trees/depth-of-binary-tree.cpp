b12f8aad4eb1e51177dd0254250e6c2f12e9c95b
Iris Hu <irishu0909@gmail.com>
2026-09-11 04:13:06 -0400

    104

---
+++ trees/depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+// https://neetcode.io/problems/depth-of-binary-tree/question?list=neetcode150
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int l = maxDepth(root->left);
+        int r = maxDepth(root->right);
+        return 1+max(l, r);
+    }
+};
