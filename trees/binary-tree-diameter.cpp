5df5a6fa0785a45799df5890c56f4c2d9cf80953
Iris Hu <irishu0909@gmail.com>
2026-09-13 18:30:43 -0400

    543

---
+++ trees/binary-tree-diameter.cpp
@@ -0,0 +1,29 @@
+// https://neetcode.io/problems/binary-tree-diameter/question
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int height(TreeNode* root, int& d) {
+        if (!root) return 0;
+        int lh = height(root->left, d);
+        int rh = height(root->right, d);
+        int diameter = lh+rh;
+        d = max(d, diameter);
+        return 1+max(lh, rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int d = 0;
+        height(root, d);
+        return d;
+    }
+};
